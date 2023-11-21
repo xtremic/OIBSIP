7839ecb18f464faf20411c8132d8bf8341b281c2
Anderson Robinson <andersonemma152@gmail.com>
2023-11-21 22:13:11 +0100

    still on working on landing page

--- Landing_page/script.js
+++ Landing_page/script.js
@@ -20,3 +20,40 @@ function nav()
         }
     
 }
+
+var isOpen = false
+
+function Cars(event){
+    var parenet = event.target
+    var cars = document.getElementsByClassName('car-unit')
+
+    if(isOpen === false){
+        cars[0].style.transform = 'translate(200px)'
+        cars[1].style.transform = 'translate(-250px)'
+
+        isOpen = true
+        
+    }else{
+        cars[0].style.transform = ''
+        cars[1].style.transform = ''
+        
+        isOpen = false
+
+    }
+    
+
+}
+
+function change(){
+    var car = document.getElementById('car')
+    car.setAttribute('src', './medis/reed car1.jpg')
+    
+}
+
+function Prev(){
+    var car = document.getElementById('car')
+    car.setAttribute('src', './medis/grey.jpeg')
+   
+}
+
+
